2821dcb229ca91f56e38d174e1dbc8f70ba802e0
Cassidy Schaufele <cschaufe@kent.edu>
2016-01-23 07:28:32 -0500

    Sound conversion libraries

--- src/main.py
+++ src/main.py
@@ -2,9 +2,35 @@ import os
 import sys
 import logging
 import json
-import http.client
+import httplib
+import urllib
+import audiotranscode
+import houndify
+import string
+import random
 from flask import Flask, Response, render_template, request, redirect, url_for, send_from_directory, g, session
 from multiprocessing.pool import ThreadPool
+
+houndClient = houndify.StreamingHoundClient("F98d-lDRNRHV91T7LFIiKqHAZauzZbsvytojjNyFqr8sdZTl_9UmMjW4joxKqTJO7w688WUi7Cl1tRCjst3WRw==",
+											"2z8RgKlFDs2paNd-opTnmQ==")
+
+
+def wav(filePath, name):
+	newName = name + ".wav"
+	at = audiotranscode.AudioTranscode()
+	at.transcode(filePath, newName)
+	return 0
+
+class MyListener(houndify.HoundListener):
+	def onPartialTranscript(self, transcript):
+		print "Partial transcript: " + transcript
+	def onFinalResponse(self, response):
+		print "Final response: " + str(response)
+	def onTranslatedResponse(self, response):
+		print "Translated response: " + response
+	def onError(self, err):
+		print "ERROR"
+
 
 
 webapp = Flask(__name__)
@@ -21,7 +47,7 @@ def root():
 @webapp.route("/api/create")
 def create(newObj):
 	data = request.method
-	connection = http.client.HTTPSConnection('api.parse.com', 443)
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
 	connection.connect()
 	connection.request('POST', '/1/classes/Feedbacks', json.dumps(data), {
 	       "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
@@ -31,7 +57,7 @@ def create(newObj):
 
 @webapp.route("/api/read")
 def read():
-	connection = http.client.HTTPSConnection('api.parse.com', 443)
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
 	connection.connect()
 	connection.request('GET', '/1/classes/Feedbacks', '', {
 	       "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
@@ -41,7 +67,7 @@ def read():
 
 @webapp.route("/api/update")
 def update():
-	connection = http.client.HTTPSConnection('api.parse.com', 443)
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
 	connection.connect()
 	connection.request('PUT', '/1/classes/Feedbacks', json.dumps({
        "score": 1337,
@@ -53,6 +79,23 @@ def update():
 	     })
 	return connection.getresponse().read()
 
+@webapp.route("/api/speech2text")
+def speech2text():
+	connection = httplib.HTTPSConnection('api.parse.com', 443)
+	connection.connect()
+	connection.request('GET', '/1/classes/Feedbacks/Vhb84o3k9P', '', {
+	       "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
+	       "X-Parse-REST-API-Key": "GokaVtTay8vWCQeydQZzC4neVhIDhz5OnsyuWd9G"
+	     })
+	# testObj = connection.getresponse().read()
+	result = json.loads(connection.getresponse().read().decode("utf-8"))
+	url = result["audioFile"]["url"]
+	unique = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
+	uuid = unique + ".m4a"
+	urllib.urlretrieve (url, uuid)
+	wav(uuid, unique)
+
+	return("success")
 
 
 #server initializiation
