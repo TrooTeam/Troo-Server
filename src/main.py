d59cf7a5aae2f5a912f120fe0600eda8ef11d852
Cassidy Schaufele <cschaufe@kent.edu>
2016-01-23 03:10:48 -0500

    Delete now utilizes delete.

--- src/main.py
+++ src/main.py
@@ -5,15 +5,8 @@ import json
 import http.client
 from flask import Flask, Response, render_template, request, redirect, url_for, send_from_directory, g, session
 from multiprocessing.pool import ThreadPool
-#
-# connection = http.client.HTTPSConnection('api.parse.com', 443)
-# connection.connect()
-# connection.request('GET', '/1/classes/Feedbacks', '', {
-#        "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
-#        "X-Parse-REST-API-Key": "GokaVtTay8vWCQeydQZzC4neVhIDhz5OnsyuWd9G"
-#      })
-# result = json.loads(connection.getresponse().read().decode("utf-8"))
-# print(result)
+connection = http.client.HTTPSConnection('api.parse.com', 443)
+connection.connect()
 
 
 webapp = Flask(__name__)
@@ -25,18 +18,38 @@ def root():
 # General format is "/api/<endpoint here>"
 @webapp.route("/api/create")
 def create():
+	connection.request('PUT', '/1/classes/Feedbacks', '', {
+	       "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
+	       "X-Parse-REST-API-Key": "GokaVtTay8vWCQeydQZzC4neVhIDhz5OnsyuWd9G"
+	     })
+	result = json.loads(connection.getresponse().read().decode("utf-8"))
 	return 0
 
 @webapp.route("/api/read")
 def read():
-	return 0
+	connection.request('GET', '/1/classes/Feedbacks', '', {
+	       "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
+	       "X-Parse-REST-API-Key": "GokaVtTay8vWCQeydQZzC4neVhIDhz5OnsyuWd9G"
+	     })
+	result = json.loads(connection.getresponse().read().decode("utf-8"))
+	return result
 
 @webapp.route("/api/update")
 def update():
+	connection.request('POST', '/1/classes/Feedbacks', '', {
+	       "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
+	       "X-Parse-REST-API-Key": "GokaVtTay8vWCQeydQZzC4neVhIDhz5OnsyuWd9G"
+	     })
+	result = json.loads(connection.getresponse().read().decode("utf-8"))
 	return 0
 
 @webapp.route("/api/delete")
-def update():
+def delete():
+	connection.request('POST', '/1/classes/Feedbacks', '', {
+	       "X-Parse-Application-Id": "cBl3nISVOAT6ryXczsTeQFAiEr0os9oYWXUJHpKb",
+	       "X-Parse-REST-API-Key": "GokaVtTay8vWCQeydQZzC4neVhIDhz5OnsyuWd9G"
+	     })
+	result = json.loads(connection.getresponse().read().decode("utf-8"))
 	return 0
 
 
