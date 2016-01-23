5b7645e5494cdd312689b03a0faed7f816648c89
Isaac Park <isaac.a.park@gmail.com>
2016-01-23 04:32:41 -0500

    Basic server

--- src/main.py
+++ src/main.py
@@ -1,27 +1,25 @@
 import os
-import sys
-import logging
 import json
-from flask import Flask, Response, render_template, request, redirect, url_for, send_from_directory, g, session
-from multiprocessing.pool import ThreadPool
+from flask import Flask, request, jsonify
 
 
 webapp = Flask(__name__)
-@webapp.route("/")
-def root():
-	return render_template('index.html')
-
-# API Routes
-# General format is "/api/<endpoint here>"
-@webapp.route("/api/structure")
-def query():
-	return 0
-@webapp.route("/api/review")
-def review():
-	return 0
 
 
-#server initializiation
+@webapp.route("/api/log_review")
+def log_review():
+    review = request.get_json()
+    print(review)
+    return jsonify({})
+
+
+@webapp.route("/api/handle_query")
+def handle_query():
+    query = request.get_json()
+    print(query)
+    return jsonify({})
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 33507))
     webapp.run(host='0.0.0.0', port=port, debug=True)
