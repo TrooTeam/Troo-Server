fce99d2330577b23fe7cfb0f5a9676e8c0485e62
Isaac Park <isaac.a.park@gmail.com>
2016-01-23 07:52:43 -0500

    Get Entity Score Array

--- src/Analize.py
+++ src/Analize.py
@@ -1,4 +1,5 @@
 from alchemyapi import AlchemyAPI
+
 alchemyapi = AlchemyAPI()
 
 
@@ -6,6 +7,16 @@ def get_key_sentences(review):
     pass
 
 
-def get_keywords(review):
-    response = alchemyapi.sentiment("text", review, {"sentiment": 1})
-    print(response["entities"])
+def get_entities(review):
+    entities = alchemyapi.keywords("text", review, {"sentiment": 1})
+
+    result = []
+    for entity in entities["keywords"]:
+        print entity
+        if "score" not in entity["sentiment"]:
+            continue
+        weighted_score = float(entity["relevance"]) * float(entity["sentiment"]["score"])
+        result.append((entity["text"], float(entity["sentiment"]["score"]), weighted_score))
+    result = sorted(result, key=lambda data: abs(data[2]), reverse=True)
+    return result
+
